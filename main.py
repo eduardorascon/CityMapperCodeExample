efdd7bebcee9d50d83f18f61e30a2251a9249d45
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-08-04 18:00:08 -0500

    MainHandler function renamed to MainPage

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
