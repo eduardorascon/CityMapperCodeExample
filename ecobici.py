6a292a2f81238dec7804c124dc34900cc66a4f12
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-08-10 12:18:03 -0500

    Added post method to save api values to the datastore.

--- ecobici.py
+++ ecobici.py
@@ -1,7 +1,7 @@
 import webapp2
 import urllib, json
 import os
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
 
 class EcobiciPage(webapp2.RequestHandler):
@@ -20,3 +20,16 @@ class EcobiciCredentialsPage(webapp2.RequestHandler):
 
         path = os.path.join(os.path.dirname(__file__), 'credentials.html')
         self.response.write(template.render(path, template_values))
+
+    def post(self):
+        entity_key = ndb.Key("EcobiciCredentials", "default_credentials")
+        credentials = EcobiciCredentials(parent = entity_key)
+        credentials.api_key = self.request.get("api_key")
+        credentials.api_secret = self.request.get("api_secret")
+        credentials.put()
+        self.redirect('/ecobici')
+
+
+class EcobiciCredentials(ndb.Model):
+    api_key = ndb.StringProperty()
+    api_secret = ndb.StringProperty()
