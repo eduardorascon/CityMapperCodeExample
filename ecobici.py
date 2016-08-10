b8d11a0db1be6ad2a1e3528c672fb809ba7d7134
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-08-10 11:07:57 -0500

    Show stations location.

--- ecobici.py
+++ ecobici.py
@@ -1,13 +1,11 @@
 import webapp2
 import urllib, json
+from google.appengine.ext import db
 
 class EcobiciPage(webapp2.RequestHandler):
     def get(self):
         url = 'http://api.labcd.mx/v1/movilidad/estaciones-ecobici'
         response = urllib.urlopen(url)
         obj = json.load(response)
-        self.response.headers['Content-Type'] = 'application/json'
         for ecobici_station in obj:
-            for attribute, value in ecobici_station.iteritems():
-                self.response.write(attribute + '::')
-            self.response.write('<br />')
+            self.response.write(ecobici_station["location"] + "<br />")
