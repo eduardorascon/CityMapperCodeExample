ff44a33275e7efb8ccebd7b61cb34657eeb3508c
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-09-01 14:42:24 -0500

    Saving status to datastore.

--- cronjobs.py
+++ cronjobs.py
@@ -1,9 +1,26 @@
-import webapp2
+import webapp2, json
 import apicalls
 
 class CronJobsPage(webapp2.RequestHandler):
 	def post(self):
 		#Request data from Ecobici API every 30 seconds.
-
+        obj = apicalls.get_ecobici_stations()
 		#Save data to DataStore
+        status_list = []
+        for station_status in obj["stationsStatus"]:
+            status = EcobiciStationStatus()
+            status.timestamp = "20160901130000"
+            station.id = station_status["id"]
+            station.status =  station_status["status"]
+            station.slots_occupied = station_status["availability"]["bikes"]
+            station.slots_available = station_status["availability"]["slots"]
+            status_list.append(status)
 
+        ndb.put_multi(status_list)
+
+class EcobiciStationStatus(ndb.Model):
+    timestamp = ndb.StringProperty()
+    station_id = ndb.IntegerProperty()
+    station_status = ndb.StringProperty()
+    slots_occupied = ndb.IntegerProperty()
+    slots_available = ndb.IntegerProperty()
